42fead0b73f1178810e4e1e2704567ea37cff0b7
vitalii-bezuhlyi <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2024-04-16 18:50:26 +0300

    Categories (#9)
    * CI: update workflow to invoke DEV deployment
    
    * CI: update pipeline to include dispatch event
    
    * CI: invoke Dev workflow once manually triggered
    
    * added categories
    
    * formatting
    
    * Updated csproj
    
    ---------
    
    Co-authored-by: Ihor Kriatov <ihor.kriatov@blackbird.io>
    Co-authored-by: bZverok <137277669+bZverok@users.noreply.github.com>

--- Apps.HTTP/HttpApplication.cs
+++ Apps.HTTP/HttpApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.HTTP;
 
-public class HttpApplication : IApplication
+public class HttpApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.Utilities];
+        set { }
+    }
+
     public string Name
     {
         get => "HTTP Application";

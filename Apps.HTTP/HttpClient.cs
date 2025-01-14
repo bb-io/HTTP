d089e1a656f43915732f7bc89117432e72a11265
RiabushenkoA <ariabushenko@blackbird.io>
2025-01-14 20:36:56 +0200

    fix

--- Apps.HTTP/HttpClient.cs
+++ Apps.HTTP/HttpClient.cs
@@ -19,6 +19,6 @@ public class HttpClient : BlackBirdRestClient
 
     protected override Exception ConfigureErrorException(RestResponse response)
     {
-           return new PluginApplicationException();
+           return new PluginApplicationException(response.Content);
     }
 }

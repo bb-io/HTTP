fd8c71eec1ce1d3bdbb95635545d992c0562987e
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-09-30 12:57:34 +0300

    fix FileResponseDto 'Index was outside the bounds of the array' exception, add mechanism to append filetype to end of filename if it's not there yet, add appsettings.json.example, add GetFile tests

--- Tests.HTTP/ActionsTests.cs
+++ Tests.HTTP/ActionsTests.cs
@@ -28,4 +28,66 @@ public class ActionsTests : TestBase
 
         Console.WriteLine(JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true }));
     }
+
+    [TestMethod]
+    public async Task GetFile_WithValidParameters_ReturnsFileWithOriginalFilename()
+    {
+        // Arrange
+        var actions = new Actions(InvocationContext, FileManager);
+        var request = new GetRequest
+        {
+            Endpoint = "/response-headers",
+            QueryParameters = "{ \"Content-Disposition\": \"attachment; filename=\\\"document.txt\\\"\" }"
+        };
+
+        // Act
+        var result = await actions.GetFile(request);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.IsNotNull(result.StatusCode);
+
+        Console.WriteLine(JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true }));
+    }
+
+    [TestMethod]
+    public async Task GetFile_WithInvalidContentDispositionHeader_ReturnsFileWithGuidFilename()
+    {
+        // Arrange
+        var actions = new Actions(InvocationContext, FileManager);
+        var request = new GetRequest
+        {
+            Endpoint = "/response-headers",
+            QueryParameters = "{ \"Content-Disposition\": \"attachment;\" }"
+        };
+
+        // Act
+        var result = await actions.GetFile(request);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.IsNotNull(result.StatusCode);
+
+        Console.WriteLine(JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true }));
+    }
+
+    [TestMethod]
+    public async Task GetFile_WithSemicolonSeparatedContentTypeHeader_ReturnsFileWithOneContentTypeHeader()
+    {
+        // Arrange
+        var actions = new Actions(InvocationContext, FileManager);
+        var request = new GetRequest
+        {
+            Endpoint = "/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+        };
+
+        // Act
+        var result = await actions.GetFile(request);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.IsNotNull(result.StatusCode);
+
+        Console.WriteLine(JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true }));
+    }
 }

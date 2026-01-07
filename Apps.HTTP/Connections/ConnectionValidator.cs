dddb1b9e54107449f9d68e29a0d07beb39e929d5
Artem Riabushenko <ariabushenko@blackbird.io>
2026-01-07 14:20:03 +0200

    Adjustments to connection validation

--- Apps.HTTP/Connections/ConnectionValidator.cs
+++ Apps.HTTP/Connections/ConnectionValidator.cs
@@ -3,33 +3,63 @@ using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Connections;
 using Blackbird.Applications.Sdk.Utils.Extensions.Sdk;
 using RestSharp;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Apps.HTTP.Connections;
 
 public class ConnectionValidator : IConnectionValidator
 {
     public async ValueTask<ConnectionValidationResponse> ValidateConnection(
-        IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders, 
+        IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders,
         CancellationToken cancellationToken)
     {
-        var credentialsProviders = authenticationCredentialsProviders as AuthenticationCredentialsProvider[] ?? authenticationCredentialsProviders.ToArray();
-        
-        var client = new HttpClient(credentialsProviders);
-        
-        var baseUrl = credentialsProviders.Get(CredNames.BaseUrl).Value;
-        var leftPart = new Uri(baseUrl).GetLeftPart(UriPartial.Authority);
-        var request = new HttpRequest(leftPart, Method.Get, credentialsProviders);
+        var creds = authenticationCredentialsProviders as AuthenticationCredentialsProvider[]
+           ?? authenticationCredentialsProviders.ToArray();
+
+        var raw = creds.Get(CredNames.BaseUrl).Value?.Trim();
+
+        if (string.IsNullOrWhiteSpace(raw))
+        {
+            return new ConnectionValidationResponse
+            {
+                IsValid = false,
+                Message = "Base URL is empty"
+            };
+        }
 
         try
         {
-            await client.ExecuteWithErrorHandling(request);
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var host = new UriBuilder(raw.Contains("://") ? raw : $"http://{raw}").Host;
+
+            if (string.IsNullOrWhiteSpace(host))
+            {
+                return new ConnectionValidationResponse
+                {
+                    IsValid = false,
+                    Message = "Invalid URL"
+                };
+            }
+
+            var addresses = await Dns.GetHostAddressesAsync(host);
+
             return new ConnectionValidationResponse
             {
-                IsValid = true,
-                Message = "Success"
+                IsValid = addresses.Length > 0,
+                Message = addresses.Length > 0 ? "Success" : "Ping failed"
             };
         }
-        catch (Exception)
+        catch (UriFormatException)
+        {
+            return new ConnectionValidationResponse
+            {
+                IsValid = false,
+                Message = "Invalid URL"
+            };
+        }
+        catch (SocketException)
         {
             return new ConnectionValidationResponse
             {

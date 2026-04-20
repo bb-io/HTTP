569130659f38556088b217f65317963782e0cd11
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-04-20 13:49:44 +0300

    use file-scoped namespace in ConnectionValidation.cs

--- Tests.HTTP/ConnectionValidation.cs
+++ Tests.HTTP/ConnectionValidation.cs
@@ -1,19 +1,18 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tests.HTTP.Base;
 
-namespace Tests.HTTP
-{
-    [TestClass]
-    public class ConnectionValidation : TestBase
-    {
-        [TestMethod]
-        public async Task ValidateConnection_Success()
-        {
-            var validator = new Apps.HTTP.Connections.ConnectionValidator();
+namespace Tests.HTTP;
 
-            var result = await validator.ValidateConnection(Creds, CancellationToken.None);
-            Console.WriteLine(result.Message);
-            Assert.IsTrue(result.IsValid);
-        }
+[TestClass]
+public class ConnectionValidation : TestBase
+{
+    [TestMethod]
+    public async Task ValidateConnection_Success()
+    {
+        var validator = new Apps.HTTP.Connections.ConnectionValidator();
+
+        var result = await validator.ValidateConnection(Creds, CancellationToken.None);
+        Console.WriteLine(result.Message);
+        Assert.IsTrue(result.IsValid);
     }
 }
